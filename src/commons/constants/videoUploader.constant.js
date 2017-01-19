0f45a1df3df09694912dcfe7d03ac217c3b8bbd9
mauroBus <maurobuselli@gmail.com>
2017-01-18 22:13:44 -0300

    Define Wistia constants

---
+++ src/commons/constants/videoUploader.constant.js
@@ -0,0 +1,15 @@
+(function() {
+  /*
+   * These constants are defined here for a demo purpose.
+   * They should not be versioned.
+   */
+
+  angular.module('VideoApp.commons')
+    .constant('VideoUpoaderConstants', {
+      PASSWORD: 'a348406281a44a62c47d1a611c9e8802005a54e42f05fb1865b1d1e77bb9ec92',
+      PROJECT_ID: 'xh6lyhls7l',
+      URL: 'https://upload.wistia.com/',
+      EMBED_URL: 'https://fast.wistia.net/embed/iframe/'
+    });
+
+})();
